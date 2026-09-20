9e1cfb0efa43a8dc31e93aa21813f98075a1aad5
atifdeveloper238 <atifoffical856@gmail.com>
2026-09-20 12:57:13 +0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL ="";
-const SUPABASE_ANON_KEY ="";
+const SUPABASE_URL ="https://kahmpvgqzxarsegnrogr.supabase.co";
+const SUPABASE_ANON_KEY ="sb_publishable_rlRMyly8iVoOlqn9MMRVow_jgjVaWtx";
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
